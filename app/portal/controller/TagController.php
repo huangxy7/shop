0e83132ae728aa4dc454aa6a92b7116a238f796a
huangxy <huangxy72@qq.com>
2019-05-22 01:22:28 +0800

    修改前端页面

--- app/portal/controller/TagController.php
+++ app/portal/controller/TagController.php
@@ -24,23 +24,6 @@ class TagController extends HomeBaseController
      */
     public function index()
     {
-        $id             = $this->request->param('id');
-
-        $portalTagModel = new PortalTagModel();
-
-        if(is_numeric($id)){
-            $tag = $portalTagModel->where('id', $id)->where('status', 1)->find();
-        }else{
-            $tag = $portalTagModel->where('name', $id)->where('status', 1)->find();
-        }
-
-
-        if (empty($tag)) {
-            abort(404, '标签不存在!');
-        }
-
-        $this->assign('tag', $tag);
-
         return $this->fetch('/tag');
     }
 
