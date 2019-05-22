68baa996707eaa2f880ca755dedad4c317ce82d1
11ze <763992263@qq.com>
2019-05-23 02:18:12 +0800

    增加

--- data/route/route.php
+++ data/route/route.php
@@ -1,25 +1,2 @@
 <?php	return array (
-  's/:id' => 
-  array (
-    0 => 'portal/Article/index?cid=1',
-    1 => 
-    array (
-    ),
-    2 => 
-    array (
-      'id' => '\\d+',
-      'cid' => '\\d+',
-    ),
-  ),
-  's' => 
-  array (
-    0 => 'portal/List/index?id=1',
-    1 => 
-    array (
-    ),
-    2 => 
-    array (
-      'id' => '\\d+',
-    ),
-  ),
 );
